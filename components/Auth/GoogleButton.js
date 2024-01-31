a110a7ace8a578d1a841d74ae81f070e524f5177
Junhyuk Seo <linejk123@inu.ac.kr>
2024-01-31 16:20:17 +0900

    authcontent 최하단에 GoogleButton 추가 외
    Auth >> GoogleButton 추가

---
+++ components/Auth/GoogleButton.js
@@ -0,0 +1,44 @@
+// import { Button, StyleSheet, Text, View } from "react-native";
+import { useContext, useState } from "react";
+import { Linking, TouchableOpacity, Image } from "react-native";
+import * as expo_Linking from "expo-linking";
+import queryString from "query-string";
+import { useNavigation } from "@react-navigation/native";
+import { AuthContext } from "../../util/auth-context";
+
+export default function GoogleButton() {
+  const navigation = useNavigation();
+
+  const handleOpenURL = ({ url }) => {
+    const query = url.split("?")[1];
+    const query_string = queryString.parse(query);
+
+    const authCtx = useContext(AuthContext); // Context 호출 변수
+
+    console.log(query_string.login);
+    if (query_string.login === "true") {
+      authCtx.authenticate(query_string.token);
+    } else {
+      navigation.navigate("Signup");
+    }
+  };
+
+  login = () => {
+    Linking.addEventListener("url", handleOpenURL);
+
+    const url = expo_Linking.createURL();
+    // const url = 123;
+    Linking.openURL(
+      `http://www.healthinu.r-e.kr:8080/auth/google?url=${url}`
+    ).catch((err) => console.error("An error occurred", err));
+  };
+
+  return (
+    <TouchableOpacity onPress={login}>
+      <Image
+        source={require("../../assets/google.png")}
+        style={{ width: 50, height: 50, alignSelf: "center" }}
+      />
+    </TouchableOpacity>
+  );
+}
