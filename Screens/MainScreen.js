ba57c547287546b469ec162d224e87969db26164
123ekrxjtj <123ekrxjtj@naver.com>
2024-03-28 02:37:00 +0900

    MainScreen 디자인 수정

--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -56,82 +56,86 @@ export default function MainScreen({ navigation }) {
       {/* HealthINU 텍스트와 이미지 들어가는 View */}
       <View style={styles.barContainer}>
         {/* HealthINU 텍스트 */}
-        <Text style={styles.text}>HealthINU</Text>
+        <Text style={{ ...styles.text, fontSize: 32 }}>HealthINU</Text>
         {/* 프로필 화면 이동 */}
+        <Image
+          source={require("../assets/sample_avatar.png")}
+          style={{ width: 64, height: 64, borderRadius: 32, margin: 16 }}
+        />
       </View>
 
       {/* 프로필 사진, 이름, 레벨, 키, 몸무게, BMI */}
       <View
         style={{
-          width: 350,
-          height: 272,
+          width: "auto",
+          height: "272",
           flexDirection: "row",
-          justifyContent: "space-around",
+          // justifyContent: "space-around",
           backgroundColor: "#1F1F1F",
-          borderRadius: 32,
-          padding: 8,
-        }}
-      >
-        {/* 프로필 사진 */}
-        <Image
-          source={require("../assets/sample_avatar.png")}
-          style={{ width: 150, height: "100%", borderRadius: 32 }}
-        />
+          borderRadius: 16,
+          marginHorizontal: 16,
+        }}>
         <View
           style={{
+            width: "100%",
+            flexDirection: "row",
             justifyContent: "space-around",
             padding: 16,
-            alignItems: "center",
-          }}
-        >
-          {/* 이름, 레벨, 키, 몸무게, BMI */}
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.text}>{user_info.user_name}</Text>
-            <Text style={styles.grayText}>nickname</Text>
-          </View>
+            // alignItems: "center",
+          }}>
+          {/* 레벨, 키, 몸무게, BMI */}
+
           {/* <Text style={styles.text}>Lv. {user_info.user_level}</Text> */}
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.text}>{user_info.user_height}cm</Text>
+          <View>
             <Text style={styles.grayText}>Height</Text>
+            <Text style={styles.text}>{user_info.user_height}cm</Text>
           </View>
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.text}>{user_info.user_weight}kg</Text>
+          <View>
             <Text style={styles.grayText}>Weight</Text>
+            <Text style={styles.text}>{user_info.user_weight}kg</Text>
           </View>
-          <View style={{ alignItems: "center" }}>
-            <Text style={styles.text}>22.02</Text>
+          <View>
             <Text style={styles.grayText}>BMI</Text>
+            <Text style={styles.text}>22.02</Text>
           </View>
         </View>
       </View>
+
+      <Text
+        style={[
+          styles.text,
+          { alignSelf: "flex-start", padding: 16, fontSize: 24 },
+        ]}>
+        Today Health
+      </Text>
       <View
         style={{
           backgroundColor: "#1F1F1F",
-          borderRadius: 32,
-          marginTop: 16,
-          width: "100%",
+          borderRadius: 16,
+          marginHorizontal: 16,
           padding: 8,
-        }}
-      >
-        <Text
-          style={[
-            styles.text,
-            { color: Colors.green2, padding: 16, fontSize: 24 },
-          ]}
-        >
-          Today Health
-        </Text>
+          height: 192,
+        }}>
         <FlatList
           data={exerciseItems}
           horizontal={true}
           renderItem={(itemData) => {
             return (
-              <View style={{ marginLeft: 15 }}>
+              <View>
                 <Image
                   source={require("../assets/madong.png")}
-                  style={{ width: 177, height: 144, borderRadius: 32 }}
+                  style={{
+                    width: 128,
+                    height: 128,
+                    borderRadius: 16,
+                    margin: 8,
+                  }}
                 />
-                <Text style={{ ...styles.text, padding: 6 }}>
+                <Text
+                  style={{
+                    ...styles.text,
+                    marginLeft: 8,
+                  }}>
                   {itemData.item.text}
                 </Text>
               </View>
@@ -147,8 +151,7 @@ export default function MainScreen({ navigation }) {
           left: 0,
           right: 0,
           bottom: 0,
-        }}
-      >
+        }}>
         {/*
         카메라 페이지로 가는 버튼
         누르면 카메라 권한 요청
